de8d1b87dcecf4a6549fa2d573370798b988b36f
edsml-kl121 <yugiohtra@gmail.com>
2022-04-16 22:50:25 +0700

    done port fo

---
+++ src/data.js
@@ -0,0 +1,66 @@
+// src/data.js
+import Bathy from "./components/images/Bathy.png"
+
+export const projects = [
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: Bathy,
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "React Tracks",
+    subtitle: "React and Python",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: Bathy,
+    link: "https://reedbarger.com",
+  },
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: Bathy,
+    link: "https://jsbootcamp.com",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: Bathy,
+    link: "https://pythonbootcamp.com",
+  },
+];
+
+export const skills = ["python", "React", "Node", "Ruby on Rails"]
+
+export const testimonials = [
+  {
+    quote : "He is the best developer in the world",
+    image : Bathy,
+    name: "Mew",
+    company: "udemy"
+  },
+  {
+    quote : "He is the best developer in the world",
+    image : Bathy,
+    name: "Mew",
+    company: "udemy"
+  },
+  {
+    quote : "He is the best developer in the world",
+    image : Bathy,
+    name: "Mew",
+    company: "udemy"
+  },
+  {
+    quote : "He is the best developer in the world",
+    image : Bathy,
+    name: "Mew",
+    company: "udemy"
+  }
+]
